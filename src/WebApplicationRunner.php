5c9806fdfe538938a65c02ff00924ae4903d4116
Evgeniy Zyubin <mail@devanych.ru>
2021-11-09 21:56:51 +0300

    Fix name of events configuration group

--- src/WebApplicationRunner.php
+++ src/WebApplicationRunner.php
@@ -43,7 +43,7 @@ final class WebApplicationRunner implements RunnerInterface
     private ?ContainerInterface $container = null;
     private ?ErrorHandler $temporaryErrorHandler = null;
     private ?string $bootstrapGroup = 'bootstrap-web';
-    private ?string $eventGroup = 'event-web';
+    private ?string $eventsGroup = 'events-web';
 
     public function __construct(string $rootPath, bool $debug, ?string $environment)
     {
@@ -66,17 +66,17 @@ final class WebApplicationRunner implements RunnerInterface
         return $new;
     }
 
-    public function withEvent(string $eventGroup): self
+    public function withEvents(string $eventsGroup): self
     {
         $new = clone $this;
-        $new->eventGroup = $eventGroup;
+        $new->eventsGroup = $eventsGroup;
         return $new;
     }
 
-    public function withoutEvent(): self
+    public function withoutEvents(): self
     {
         $new = clone $this;
-        $new->eventGroup = null;
+        $new->eventsGroup = null;
         return $new;
     }
 
@@ -137,9 +137,9 @@ final class WebApplicationRunner implements RunnerInterface
             $this->runBootstrap($container, $config->get($this->bootstrapGroup));
         }
 
-        if ($this->debug && $this->eventGroup !== null) {
+        if ($this->debug && $this->eventsGroup !== null) {
             /** @psalm-suppress MixedMethodCall */
-            $container->get(ListenerConfigurationChecker::class)->check($config->get($this->eventGroup));
+            $container->get(ListenerConfigurationChecker::class)->check($config->get($this->eventsGroup));
         }
 
         /** @var Application */
