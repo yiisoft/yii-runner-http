eecfd158a03f7135e6164e021accfee2070c1477
devanych <mail@devanych.ru>
2022-01-15 21:16:37 +0300

    Update due yiisoft/yii-runner changes

--- src/HttpApplicationRunner.php
+++ src/HttpApplicationRunner.php
@@ -11,13 +11,10 @@ use Psr\Container\NotFoundExceptionInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Throwable;
-use Yiisoft\Config\ConfigInterface;
-use Yiisoft\Config\ConfigPaths;
 use Yiisoft\Definitions\Exception\CircularReferenceException;
 use Yiisoft\Definitions\Exception\InvalidConfigException;
 use Yiisoft\Definitions\Exception\NotInstantiableException;
 use Yiisoft\Di\Container;
-use Yiisoft\Di\ContainerConfig;
 use Yiisoft\Di\NotFoundException;
 use Yiisoft\ErrorHandler\ErrorHandler;
 use Yiisoft\ErrorHandler\Middleware\ErrorCatcher;
@@ -25,29 +22,19 @@ use Yiisoft\ErrorHandler\Renderer\HtmlRenderer;
 use Yiisoft\Http\Method;
 use Yiisoft\Log\Logger;
 use Yiisoft\Log\Target\File\FileTarget;
-use Yiisoft\Yii\Event\ListenerConfigurationChecker;
 use Yiisoft\Yii\Http\Application;
 use Yiisoft\Yii\Http\Handler\ThrowableHandler;
-use Yiisoft\Yii\Runner\BootstrapRunner;
-use Yiisoft\Yii\Runner\ConfigFactory;
+use Yiisoft\Yii\Runner\ApplicationRunner;
 use Yiisoft\Yii\Runner\Http\Exception\HeadersHaveBeenSentException;
-use Yiisoft\Yii\Runner\RunnerInterface;
 
 use function microtime;
 
 /**
  * `HttpApplicationRunner` runs the Yii HTTP application.
  */
-final class HttpApplicationRunner implements RunnerInterface
+final class HttpApplicationRunner extends ApplicationRunner
 {
-    private bool $debug;
-    private string $rootPath;
-    private ?string $environment;
-    private ?ConfigInterface $config = null;
-    private ?ContainerInterface $container = null;
     private ?ErrorHandler $temporaryErrorHandler = null;
-    private ?string $bootstrapGroup = 'bootstrap-web';
-    private ?string $eventsGroup = 'events-web';
 
     /**
      * @param string $rootPath The absolute path to the project root.
@@ -56,93 +43,9 @@ final class HttpApplicationRunner implements RunnerInterface
      */
     public function __construct(string $rootPath, bool $debug, ?string $environment)
     {
-        $this->rootPath = $rootPath;
-        $this->debug = $debug;
-        $this->environment = $environment;
-    }
-
-    /**
-     * Returns a new instance with the specified bootstrap configuration group name.
-     *
-     * @param string $bootstrapGroup The bootstrap configuration group name.
-     *
-     * @return self
-     */
-    public function withBootstrap(string $bootstrapGroup): self
-    {
-        $new = clone $this;
-        $new->bootstrapGroup = $bootstrapGroup;
-        return $new;
-    }
-
-    /**
-     * Returns a new instance and disables the use of bootstrap configuration group.
-     *
-     * @return self
-     */
-    public function withoutBootstrap(): self
-    {
-        $new = clone $this;
-        $new->bootstrapGroup = null;
-        return $new;
-    }
-
-    /**
-     * Returns a new instance with the specified configuration group of events name for check.
-     *
-     * Note: The configuration of events is checked only in debug mode.
-     *
-     * @param string $eventsGroup The configuration group name of events for check.
-     *
-     * @return self
-     */
-    public function withCheckingEvents(string $eventsGroup): self
-    {
-        $new = clone $this;
-        $new->eventsGroup = $eventsGroup;
-        return $new;
-    }
-
-    /**
-     * Returns a new instance and disables checking of the event configuration group.
-     *
-     * Note: The configuration of events is checked only in debug mode.
-     *
-     * @return self
-     */
-    public function withoutCheckingEvents(): self
-    {
-        $new = clone $this;
-        $new->eventsGroup = null;
-        return $new;
-    }
-
-    /**
-     * Returns a new instance with the specified config instance {@see ConfigInterface}.
-     *
-     * @param ConfigInterface $config The config instance.
-     *
-     * @return self
-     */
-    public function withConfig(ConfigInterface $config): self
-    {
-        $new = clone $this;
-        $new->config = $config;
-        return $new;
-    }
-
-    /**
-     * Returns a new instance with the specified container instance {@see ContainerInterface}.
-     *
-     * @param ContainerInterface $container The container instance.
-     *
-     * @return self
-     */
-    public function withContainer(ContainerInterface $container): self
-    {
-        $new = clone $this;
-        $new->container = $container;
-        return $new;
+        parent::__construct($rootPath, $debug, $environment);
+        $this->bootstrapGroup = 'bootstrap-web';
+        $this->eventsGroup = 'events-web';
     }
 
     /**
@@ -176,8 +79,8 @@ final class HttpApplicationRunner implements RunnerInterface
         $temporaryErrorHandler = $this->createTemporaryErrorHandler();
         $this->registerErrorHandler($temporaryErrorHandler);
 
-        $config = $this->config ?? ConfigFactory::create(new ConfigPaths($this->rootPath, 'config'), $this->environment);
-        $container = $this->container ?? $this->createDefaultContainer($config);
+        $config = $this->config ?? $this->createConfig();
+        $container = $this->container ?? $this->createContainer($config, 'web');
 
         // Register error handler with real container-configured dependencies.
         /** @var ErrorHandler $actualErrorHandler */
@@ -188,15 +91,8 @@ final class HttpApplicationRunner implements RunnerInterface
             $container = $container->get(ContainerInterface::class);
         }
 
-        // Run bootstrap
-        if ($this->bootstrapGroup !== null) {
-            $this->runBootstrap($container, $config->get($this->bootstrapGroup));
-        }
-
-        if ($this->debug && $this->eventsGroup !== null) {
-            /** @psalm-suppress MixedMethodCall */
-            $container->get(ListenerConfigurationChecker::class)->check($config->get($this->eventsGroup));
-        }
+        $this->runBootstrap($config, $container);
+        $this->checkEvents($config, $container);
 
         /** @var Application $application */
         $application = $container->get(Application::class);
@@ -224,28 +120,6 @@ final class HttpApplicationRunner implements RunnerInterface
             $application->afterEmit($response ?? null);
             $application->shutdown();
         }
-    }
-
-    /**
-     * @throws ErrorException|InvalidConfigException
-     */
-    private function createDefaultContainer(ConfigInterface $config): Container
-    {
-        $containerConfig = ContainerConfig::create()->withValidate($this->debug);
-
-        if ($config->has('web')) {
-            $containerConfig = $containerConfig->withDefinitions($config->get('web'));
-        }
-
-        if ($config->has('providers-web')) {
-            $containerConfig = $containerConfig->withProviders($config->get('providers-web'));
-        }
-
-        if ($config->has('delegates-web')) {
-            $containerConfig = $containerConfig->withDelegates($config->get('delegates-web'));
-        }
-
-        return new Container($containerConfig);
     }
 
     private function createTemporaryErrorHandler(): ErrorHandler
@@ -278,10 +152,5 @@ final class HttpApplicationRunner implements RunnerInterface
         }
 
         $registered->register();
-    }
-
-    private function runBootstrap(ContainerInterface $container, array $bootstrapList): void
-    {
-        (new BootstrapRunner($container, $bootstrapList))->run();
     }
 }
