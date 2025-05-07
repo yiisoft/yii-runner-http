aa901b8c507629f5ca0314e7af2bc541cfaeb559
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-05-07 10:33:35 +0300

    phpdoc

--- src/EmitterInterface.php
+++ src/EmitterInterface.php
@@ -7,14 +7,21 @@ namespace Yiisoft\Yii\Runner\Http;
 use Psr\Http\Message\ResponseInterface;
 use Yiisoft\Yii\Runner\Http\Exception\HeadersHaveBeenSentException;
 
+/**
+ * `EmitterInterface` is responsible for sending HTTP responses.
+ */
 interface EmitterInterface
 {
     /**
+     * Sends the response to the client.
+     *
      * @throws HeadersHaveBeenSentException
      */
     public function emit(ResponseInterface $response): void;
 
     /**
+     * Sends only the headers of the response to the client.
+     *
      * @throws HeadersHaveBeenSentException
      */
     public function emitHeaders(ResponseInterface $response): void;
