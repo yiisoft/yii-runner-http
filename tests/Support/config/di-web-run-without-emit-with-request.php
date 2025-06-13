e155870642fafef56f846d3c9d061f8471f8defd
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-06-13 11:13:14 +0300

    test

---
+++ tests/Support/config/di-web-run-without-emit-with-request.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use HttpSoft\Message\Response;
+use HttpSoft\Message\StreamFactory;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+return [
+    'applicationMiddleware' => new class () implements MiddlewareInterface {
+        public function process(
+            ServerRequestInterface $request,
+            RequestHandlerInterface $handler
+        ): ResponseInterface {
+            return (new Response())
+                ->withBody((new StreamFactory())->createStream($request->getHeaderLine('X-CONTENT')));
+        }
+    },
+];
